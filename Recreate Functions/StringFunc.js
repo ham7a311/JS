c86972fda51e06ffc1cef313cbdb3bf706633c54
ham7a311 <hamzataj371@gmail.com>
2024-12-12 11:53:23 +0400

    Update StringFunc.js

--- Recreate Functions/StringFunc.js
+++ Recreate Functions/StringFunc.js
@@ -7,6 +7,7 @@ String.prototype.myAnchor = function (name) {
     return `<a name="${name}">${this}</a>`;
 }
 
+
 String.prototype.myCharAt = function (index) {
 
     if (index < 0 || index >= this.length) {
@@ -14,4 +15,51 @@ String.prototype.myCharAt = function (index) {
     }
 
     return this[index];
+}
+
+
+String.prototype.myConcat = function (str) {
+    return this + str;
+}
+
+
+String.prototype.myEndsWith = function (str, position = 0) {
+    if (str.length > this.length) {
+        return false;
+    }
+
+    position = Math.max(0, Math.min(position, this.length));
+
+    for (let i = position; i < str.length; i++) {
+        if (this[position - str.length + i] !== str[i]) {
+            return false;
+        }
+        
+    }
+
+    return true;
+}
+
+
+String.prototype.myIndexOf = function (str, position = 0) {
+    if (typeof str !== "string" || str.length > this.length) {
+        return -1;
+    }
+
+    position = Math.max(0, Math.min(position, this.length));
+
+    for (let i = position; i <= this.length; i++) {
+        let found = true;
+        for (let j = 0; j < str.length; j++) {
+            if (this[i + j] !== str[j]) {
+                found = false;
+                break;
+            }
+        }
+        if (found) {
+            return i;
+        };
+    };
+
+    return -1;
 }
