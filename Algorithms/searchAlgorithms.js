939249a664f873ecc9b9232a8260a2d8a3b43202
ham7a311 <hamzataj371@gmail.com>
2024-12-18 11:36:45 +0400

    Update searchAlgorithms.js

--- Algorithms/searchAlgorithms.js
+++ Algorithms/searchAlgorithms.js
@@ -1,8 +1,16 @@
+/*                                                                  JS Algorithms
+
+Linear search and Binary search(itterative and recursive)
+
+*/
+
+
+
 function linear(arr, target) {
     for (let i = 0; i < arr.length; i++) {
         if (arr[i] === target) {
             return i;
         }
     }
-    return -1;
+    return -1; //  O(n)
 }
