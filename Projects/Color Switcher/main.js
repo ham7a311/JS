0488ac71684476c2fdd77a41a8acfe4bc0c69c34
ham7a311 <hamzataj371@gmail.com>
2024-12-20 20:44:21 +0400

    Create main.js

---
+++ Projects/Color Switcher/main.js
@@ -0,0 +1,21 @@
+const el = document.querySelectorAll("#switcher li");
+const classList = [];
+
+for (let i = 0; i < el.length; i++) {
+    classList.push(el[i].getAttribute("data-color"));
+
+    el[i].addEventListener("click", function () {
+
+        document.body.classList.remove(...classList);
+
+        document.body.classList.add(this.getAttribute("data-color"));
+
+        document.body.style.backgroundColor = document.body.classList;
+
+        localStorage.setItem("pageColor", this.getAttribute("data-color"));
+
+
+    }, false)
+}
+
+document.body.style.backgroundColor = localStorage.getItem("pageColor");
